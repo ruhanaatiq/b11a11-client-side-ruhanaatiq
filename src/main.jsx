2695cbe1e56fe496611e0747e25749efe5916705
ruhanaatiq <ruhana.atiq@gmail.com>
2025-06-03 21:28:20 +0600

    added navbar,footer components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"; 
+import MainLayout from "./layouts/MainLayout.jsx";
 
-createRoot(document.getElementById('root')).render(
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />, // 🌟 Shared layout with Navbar
+    children: [
+{ path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "*", element: <ErrorPage /> }, // 404 page
+]);
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+    <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
