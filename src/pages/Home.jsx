33e1f39e3c48cdc915e552698c03481008e9a5d5
ruhanaatiq <ruhana.atiq@gmail.com>
2025-08-12 23:10:37 +0600

    customer reviews showing twice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Banner from '../components/Banner';
 import RecentListings from '../components/RecentListings';
 import SpecialOffers from '../components/SpecialOffers';
 import WhyChooseUs from '../components/WhyChooseUs';
-import CustomerReviews from '../components/CustomerReviews';
+
 
 const Home = () => {
   return (
@@ -11,7 +11,7 @@ const Home = () => {
       <WhyChooseUs></WhyChooseUs>
       <RecentListings></RecentListings>
       <SpecialOffers></SpecialOffers>
-      <CustomerReviews></CustomerReviews>
+      
 
     </div>
   );
